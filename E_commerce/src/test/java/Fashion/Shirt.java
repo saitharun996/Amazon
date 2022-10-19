edd0887182b152268842c7acca7d6a10025eb31e
saitharun996 <saitharun996@gmail.com>
2022-10-19 16:27:56 +0530

    Unstaged to staged

---
+++ E_commerce/src/test/java/Fashion/Shirt.java
@@ -0,0 +1,17 @@
+package Fashion;
+
+public class Shirt {
+
+	public static void main(String[] args) 
+	{
+		int a=12,b=13;
+		int temp=a;
+		  a=b;
+		 b=temp;
+		System.out.println("The value of A after swapping is "+a);
+		System.out.println("The value of B after swapping is "+b);
+		
+
+	}
+
+}
