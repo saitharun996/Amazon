8a98b9b670f08dfc84bcdc131af19ecb5c9af30a
saitharun996 <saitharun996@gmail.com>
2022-10-20 15:57:08 +0530

    Harish

--- E_commerce/src/test/java/Mobile/Oneplus.java
+++ E_commerce/src/test/java/Mobile/Oneplus.java
@@ -4,6 +4,7 @@ public class Oneplus {
 
 	public static void main(String[] args) {
 		System.out.println("Oneplus mobile");
+		System.out.println("Harish and satish both are best friends");
 	}
 
 }
