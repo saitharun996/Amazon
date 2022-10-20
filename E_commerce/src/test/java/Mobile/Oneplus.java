db86a822735aa4272d3371d599bf0db9121018e1
Harish246 <srihari224466@gmail.com>
2022-10-20 16:21:20 +0530

    changes done by Harish

--- E_commerce/src/test/java/Mobile/Oneplus.java
+++ E_commerce/src/test/java/Mobile/Oneplus.java
@@ -4,11 +4,9 @@ public class Oneplus {
 
 	public static void main(String[] args) {
 		System.out.println("Oneplus mobile");
-<<<<<<< HEAD
+
 		System.out.println("want extension ram to your mobile");
-=======
-		System.out.println("Harish and satish both are best friends");
->>>>>>> 8a98b9b670f08dfc84bcdc131af19ecb5c9af30a
+
 	}
 
 }
